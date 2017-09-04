bb0ff042f2a817be57d93590762e75658716fc42
jiangzd <39627020@qq.com>
2017-09-04 12:16:37 +0800

    加入Swagger2，自动生成Restful API文档

--- java/springboot/hellospring/src/main/java/com/jzd1997/hellospring/JsonController.java
+++ java/springboot/hellospring/src/main/java/com/jzd1997/hellospring/JsonController.java
@@ -6,14 +6,25 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class JsonController {
+	
+    @ApiOperation(value="获取JSON String", notes="notes:获取JSON String")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
 	@RequestMapping(value="/jsonstring")
     public String index(String name) {
-		int i = 12/0;
         return "json string";
     }
 	
+    @ApiOperation(value="通过Map返回JSON String", notes="notes:返回Map的JSON String")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })
 	@RequestMapping("/jsonmap")
 	public Map<String,Object> json(){
 		Map<String,Object> result = new HashMap<String,Object>();
--- java/springboot/hellospring/src/main/java/com/jzd1997/hellospring/task/ScheduleTask.java
+++ java/springboot/hellospring/src/main/java/com/jzd1997/hellospring/task/ScheduleTask.java
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduleTask {
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-    @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() {
-        System.out.println("每隔5秒执行：" + dateFormat.format(new Date()));
-    }
-
-    @Scheduled(cron="*/5 * * * * *")
-    public void execByCron() {
-        System.out.println("Crontab每隔5秒执行：" + dateFormat.format(new Date()));
-    }
+//    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+//    @Scheduled(fixedRate = 5000)
+//    public void reportCurrentTime() {
+//        System.out.println("每隔5秒执行：" + dateFormat.format(new Date()));
+//    }
+//
+//    @Scheduled(cron="*/5 * * * * *")
+//    public void execByCron() {
+//        System.out.println("Crontab每隔5秒执行：" + dateFormat.format(new Date()));
+//    }
 }
